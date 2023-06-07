31fec3f755ea5dd7c098740a92621fcd4756a7b1
shinjiiiiiii <udauvergne@edenschool.fr>
2023-06-07 09:50:14 +0200

    10 premier

---
+++ app.js
@@ -0,0 +1,54 @@
+// let i = 1
+
+// while(i < 10){
+//     i++
+//     console.log(i);
+// }
+
+// for(let i = 0; i<= 100; i++){
+//     if(i% 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// for(let i = 0; i<10; i++){
+//     console.log(i*7);
+// }
+
+// for(let i = 0; i<10; i++){
+
+//     for(let a = 0; a<10; a++){
+//         console.log(i*a);
+//     } 
+// }
+
+// let res = 1;
+
+// for (let i = 2; i < 10; i++) {
+//   res = res * i;
+//   console.log(res);
+// }
+
+// let res = 0
+// for(let i = 10; i<30; i++){
+//     if(i %2 === 0){
+//         res += i
+//         console.log(res);
+//     }
+// } 
+
+// function celsius(e){
+//     console.log((e* 9/5) + 32);
+// } celsius(20)
+
+// function fahrenheit(e){
+//    console.log((e - 32) * 5/9);
+// } fahrenheit (68)
+
+let arr = [1,4,45,2]
+
+// let res = 0
+// for(let i= 0; i< arr.length; i++){
+//     res += arr[i]
+//     console.log(res);
+// }
