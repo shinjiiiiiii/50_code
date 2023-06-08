42a173040deb2887f3e99d9d4825874e10eb641d
shinjiiiiiii <udauvergne@edenschool.fr>
2023-06-08 15:51:58 +0200

    20

--- app.js
+++ app.js
@@ -5,16 +5,20 @@
 //     console.log(i);
 // }
 
+
+//2
 // for(let i = 0; i<= 100; i++){
 //     if(i% 2 === 0){
 //         console.log(i)
 //     }
 // }
 
+//3
 // for(let i = 0; i<10; i++){
 //     console.log(i*7);
 // }
 
+//4
 // for(let i = 0; i<10; i++){
 
 //     for(let a = 0; a<10; a++){
@@ -22,6 +26,7 @@
 //     } 
 // }
 
+//5
 // let res = 1;
 
 // for (let i = 2; i < 10; i++) {
@@ -29,6 +34,7 @@
 //   console.log(res);
 // }
 
+//6
 // let res = 0
 // for(let i = 10; i<30; i++){
 //     if(i %2 === 0){
@@ -37,18 +43,84 @@
 //     }
 // } 
 
+//7
 // function celsius(e){
 //     console.log((e* 9/5) + 32);
 // } celsius(20)
 
+//9
 // function fahrenheit(e){
 //    console.log((e - 32) * 5/9);
 // } fahrenheit (68)
 
-let arr = [1,4,45,2]
+//10
+// let arr = [1,4,45,2]
 
 // let res = 0
 // for(let i= 0; i< arr.length; i++){
 //     res += arr[i]
 //     console.log(res);
 // }
+
+//11
+// let arr = [1,4,45,2]
+// let res = 0
+// for(let i = 0; i<arr.length; i++ ){
+//     res += arr[i]
+//     console.log(res/arr.length);
+// }
+
+//12
+// function array(event){
+//     let arr = []
+//     for(let i = 0; i < event.length; i++){
+//         if(event[i] > 0{
+//             arr.push(event[i])
+//             console.log(arr);
+//         }
+//     }
+// }array([2,4,7,80,9,45,2])
+
+//13
+// let arr = [2,4,1,8,9,6,4,45,7,5]
+
+// let res = arr[0]
+
+// for(let i = 1; i <arr.length ;i++){
+//     if(arr[i] > res){
+//         res = arr[i]
+//         console.log(res);
+//     } 
+// }
+
+//14
+
+// let n1 = 0;
+// let n2 = 1;
+// let res = 0;
+
+//     for(let i = 2; i < 12; i++){
+//         res = n1 + n2; 
+//         n1 = n2; 
+//         n2 = res; 
+//         console.log(res);
+//      }
+
+//16
+
+// function isprime(event) {
+//     for(let i = 2; i < event; i++)
+//       if(event%i === 0) return false;
+//     return event > 1;
+//   }
+  
+//   console.log(isprime(17));
+
+//20
+
+// let arr = [1,2,3,4]
+// let left = arr.shift()
+// arr.push(left)
+
+// console.log(arr);
+
